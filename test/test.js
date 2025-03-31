dc81c68bdf3043cf50b9ceb62739af426888d1a1
Ampaire Irene <ampaireirene7@gmail.com>
2025-03-31 18:24:39 +0300

    cycle 1
    with no multiplication function and so the test didn't pass.

--- test/test.js
+++ test/test.js
@@ -1,2 +1,14 @@
 import {assert} from "chai";
-import {describe, it} from "node:test"
+import multiply from "../multiply.js";
+import {describe, it} from "node:test";
+
+describe('multiplication test', function(){
+    
+    it('check that the product of 1 and 1 is 1', function (){
+         // inside here assert is used
+         assert.equal(multiply(1,1), 1);
+
+    });
+    
+   })
+
