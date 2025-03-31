020dacdcecb4a0d49242c9d965065c05b947072b
Ampaire Irene <ampaireirene7@gmail.com>
2025-03-31 22:52:00 +0300

    cyclce 2

--- test/test.js
+++ test/test.js
@@ -2,13 +2,22 @@ import {assert} from "chai";
 import multiply from "../multiply.js";
 import {describe, it} from "node:test";
 
-describe('multiplication test', function(){
+// describe('multiplication test 1', function(){
     
-    it('check that the product of 1 and 1 is 1', function (){
-         // inside here assert is used
-         assert.equal(multiply(1,1), 1);
+//     it('check that the product of 1 and 1 is 1', function (){
+//          // inside here assert is used
+//          assert.equal(multiply(1,1), 1);
 
-    });
+//     });
     
+    describe('multiplication test 2', function(){
+    
+     it('check that the product of 2 and 2 is 4', function (){
+          // inside here assert is used
+          assert.equal(multiply(2,2), 4);
+ 
+     });
    })
+
+   
 
