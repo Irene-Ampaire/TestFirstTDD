a3a7e100ad33277f288ef4d282d2037b872aad13
Ampaire Irene <ampaireirene7@gmail.com>
2025-04-01 23:07:35 +0300

    added a factorial  function using recursion

--- factorial.js
+++ factorial.js
@@ -1,26 +1,43 @@
+// // Ampaire Irene
+// // Atim Monica
+
+// function factorial (number){
+
+// // checking if number is negative
+// if (number < 0) {
+//     console.log('Error! Factorial for negative number does not exist.');
+// }
+
+// // if number is 0
+// else if (number === 0) {
+//     console.log(`The factorial of ${number} is 1.`);
+// }
+
+// // if number is positive
+// else {
+//     let fact = 1;
+//     for (let i = 1; i <= number; i++) {
+//         fact *= i;
+//     }
+//     console.log(`The factorial of ${number} is ${fact}.`);
+// }
+// }
+
 // Ampaire Irene
 // Atim Monica
 
-function factorial (number){
+// factorial using recursion(function calling a function)
+function factorial(n){
+    if (n==0)
+        {
+            console.log(`The factorial of ${number} is 1.`);
+            }
+            
+    else {
+        let fact = n*factorial(n-1);
 
-// checking if number is negative
-if (number < 0) {
-    console.log('Error! Factorial for negative number does not exist.');
-}
+console.log(`The factorial of ${number} is ${fact}.`);
+} ;
 
-// if number is 0
-else if (number === 0) {
-    console.log(`The factorial of ${number} is 1.`);
 }
-
-// if number is positive
-else {
-    let fact = 1;
-    for (let i = 1; i <= number; i++) {
-        fact *= i;
-    }
-    console.log(`The factorial of ${number} is ${fact}.`);
-}
-}
-
-export default factorial;
+// export default factorial;
