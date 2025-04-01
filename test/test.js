ce50daa4d506f2790c0ace57fb1f3e1826848ef1
Ampaire Irene <ampaireirene7@gmail.com>
2025-04-01 16:36:20 +0300

    factorial
    added factorial function using for loop

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-import {assert} from "chai";
-import multiply from "../multiply.js";
-import {describe, it} from "node:test";
+// import {assert} from "chai";
+// import multiply from "../multiply.js";
+// import {describe, it} from "node:test";
 
 // describe('multiplication test 1', function(){
     
@@ -10,38 +10,48 @@ import {describe, it} from "node:test";
 
 //     });
     
-//     describe('multiplication test 2', function(){
+// //     describe('multiplication test 2', function(){
     
 //      it('check that the product of 2 and 2 is 4', function (){
 //           // inside here assert is used
 //           assert.equal(multiply(2,2), 4);
  
 //      });
-//    });
-//    describe('multiplication test 3', function(){
-    
+   
 //      it('check that the product of 3 and 3 is 9', function (){
 //           // inside here assert is used
 //           assert.equal(multiply(3,3), 9);
  
 //      });
 
-//    })
-// describe('multiplication test 4', function(){
-    
+
 //      it('check that the product of 4 and 4 is 16', function (){
 //           // inside here assert is used
 //           assert.equal(multiply(4,4), 16);
  
 //      });
 
-//    })
-describe('multiplication test 5', function(){
-    
-     it('check that the product of 23 and 45 is 23*45', function (){
-          // inside here assert is used
-          assert.equal(multiply(23,45), 23*45);
  
-     });
+//      it('check that the product of 23 and 45 is 23*45', function (){
+//           // inside here assert is used
+//           assert.equal(multiply(23,45), 23*45);
+ 
+//      });
 
-   })
+//    })
+
+// Ampaire Irene
+// Atim Monica
+   import {assert} from "chai";
+   import factorial from "../factorial.js";
+   import {describe, it} from "node:test";
+   
+   describe('factorial of a number', function(){
+       
+       it('check that the factorial of 4 is 8', function (){
+            // inside here assert is used
+            assert.equal(factorial(4), 24);
+   
+       });
+     })
+
