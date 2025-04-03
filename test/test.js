902ac6e50879440f9b1a48e15582e5d6542eb4da
ATIM-MONICA <monicadeann.ja@gmail.com>
2025-04-03 18:00:09 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,44 +1,44 @@
-// import {assert} from "chai";
-// import multiply from "../multiply.js";
-// import {describe, it} from "node:test";
+import {assert} from "chai";
+import multiply from "../multiply.js";
+import {describe, it} from "node:test";
 
-// describe('multiplication test 1', function(){
+describe('multiplication test 1', function(){
     
-//     it('check that the product of 1 and 1 is 1', function (){
-//          // inside here assert is used
-//          assert.equal(multiply(1,1), 1);
+    it('check that the product of 1 and 1 is 1', function (){
+         // inside here assert is used
+         assert.equal(multiply(1,1), 1);
 
-//     });
+    });
     
-// //     describe('multiplication test 2', function(){
+//     describe('multiplication test 2', function(){
     
-//      it('check that the product of 2 and 2 is 4', function (){
-//           // inside here assert is used
-//           assert.equal(multiply(2,2), 4);
+     it('check that the product of 2 and 2 is 4', function (){
+          // inside here assert is used
+          assert.equal(multiply(2,2), 4);
  
-//      });
+     });
    
-//      it('check that the product of 3 and 3 is 9', function (){
-//           // inside here assert is used
-//           assert.equal(multiply(3,3), 9);
+     it('check that the product of 3 and 3 is 9', function (){
+          // inside here assert is used
+          assert.equal(multiply(3,3), 9);
  
-//      });
+     });
 
 
-//      it('check that the product of 4 and 4 is 16', function (){
-//           // inside here assert is used
-//           assert.equal(multiply(4,4), 16);
+     it('check that the product of 4 and 4 is 16', function (){
+          // inside here assert is used
+          assert.equal(multiply(4,4), 16);
  
-//      });
+     });
 
  
-//      it('check that the product of 23 and 45 is 23*45', function (){
-//           // inside here assert is used
-//           assert.equal(multiply(23,45), 23*45);
+     it('check that the product of 23 and 45 is 23*45', function (){
+          // inside here assert is used
+          assert.equal(multiply(23,45), 23*45);
  
-//      });
+     });
 
-//    })
+   })
 
 // Ampaire Irene
 // Atim Monica
@@ -55,19 +55,19 @@
 //        });
 //      })
 
-     // Ampaire Irene
-     // Atim Monica
-        import {assert} from "chai";
-        import fibonacci from "../fibonacci.js";
-        import {describe, it} from "node:test";
+//      // Ampaire Irene
+//      // Atim Monica
+//       //   import {assert} from "chai";
+//       //   import fibonacci from "../fibonacci.js";
+//       //   import {describe, it} from "node:test";
         
-        describe('fibonacci of a number', function(){
+//       //   describe('fibonacci of a number', function(){
             
-            it('check that the fibonacci of 4 is [0, 1, 1, 2]', function (){
-               const expected = [0, 1, 1, 2];
-               const actual = fibonacci(4);
-               assert.deepStrictEqual(actual, expected); // Use deepStrictEqual for array comparison
+//       //       it('check that the fibonacci of 4 is [0, 1, 1, 2]', function (){
+//       //          const expected = [0, 1, 1, 2];
+//       //          const actual = fibonacci(4);
+//       //          assert.deepStrictEqual(actual, expected); // Use deepStrictEqual for array comparison
           
-            });
-          })
+//       //       });
+//       //     })
      
