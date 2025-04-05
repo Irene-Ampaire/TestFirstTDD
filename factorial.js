71d9cde7bf7ea3ad16900c9d636d73105a4d283e
Ampaire Irene <ampaireirene7@gmail.com>
2025-04-05 11:32:46 +0300

    factorial first fail test

--- factorial.js
+++ factorial.js
@@ -1,43 +1,43 @@
 // // Ampaire Irene
 // // Atim Monica
 
-// function factorial (number){
+function factorial (number){
 
-// // checking if number is negative
-// if (number < 0) {
-//     console.log('Error! Factorial for negative number does not exist.');
-// }
+// checking if number is negative
+if (number < 0) {
+    console.log('Error! Factorial for negative number does not exist.');
+}
 
-// // if number is 0
-// else if (number === 0) {
-//     console.log(`The factorial of ${number} is 1.`);
-// }
+// if number is 0
+else if (number === 0) {
+    console.log(`The factorial of ${number} is 1.`);
+}
 
-// // if number is positive
-// else {
-//     let fact = 1;
-//     for (let i = 1; i <= number; i++) {
-//         fact *= i;
-//     }
-//     console.log(`The factorial of ${number} is ${fact}.`);
-// }
-// }
+// if number is positive
+else {
+    let fact = 1;
+    for (let i = 1; i <= number; i++) {
+        fact *= i;
+    }
+    console.log(`The factorial of ${number} is ${fact}.`);
+}
+}
 
 // Ampaire Irene
 // Atim Monica
 
 // factorial using recursion(function calling a function)
-function factorial(n){
-    if (n==0)
-        {
-            console.log(`The factorial of ${number} is 1.`);
-            }
+// function factorial(n){
+//     if (n==0)
+//         {
+//             console.log(`The factorial of ${number} is 1.`);
+//             }
             
-    else {
-        let fact = n*factorial(n-1);
+//     else {
+//         let fact = n*factorial(n-1);
 
-console.log(`The factorial of ${number} is ${fact}.`);
-} ;
+// console.log(`The factorial of ${number} is ${fact}.`);
+// } ;
 
-}
-// export default factorial;
+// }
+export default factorial;
--- test/test.js
+++ test/test.js
@@ -1,63 +1,63 @@
-import {assert} from "chai";
-import multiply from "../multiply.js";
-import {describe, it} from "mocha";
+// import {assert} from "chai";
+// import multiply from "../multiply.js";
+// import {describe, it} from "mocha";
 
-describe('multiplication test 1', function(){
+// describe('multiplication test 1', function(){
     
-    it('check that the product of 1 and 1 is 1', function (){
-         // inside here assert is used
-         assert.equal(multiply(1,1), 1);
+//     it('check that the product of 1 and 1 is 1', function (){
+//          // inside here assert is used
+//          assert.equal(multiply(1,1), 1);
 
-    });
+//     });
     
-     it('check that the product of 2 and 2 is 4', function (){
-          // inside here assert is used
-          assert.equal(multiply(2,2), 4);
+//      it('check that the product of 2 and 2 is 4', function (){
+//           // inside here assert is used
+//           assert.equal(multiply(2,2), 4);
  
-     });
+//      });
      
    
-     it('check that the product of 3 and 3 is 9', function (){
-          // inside here assert is used
-          assert.equal(multiply(3,3), 9);
+//      it('check that the product of 3 and 3 is 9', function (){
+//           // inside here assert is used
+//           assert.equal(multiply(3,3), 9);
    
-     });
+//      });
 
 
-     it('check that the product of 4 and 4 is 16', function (){
-          // inside here assert is used
-          assert.equal(multiply(4,4), 16);
+//      it('check that the product of 4 and 4 is 16', function (){
+//           // inside here assert is used
+//           assert.equal(multiply(4,4), 16);
      
-     });
+//      });
 
  
-     it('check that the product of 23 and 45 is 23*45', function (){
-          // inside here assert is used
-          assert.equal(multiply(23,45), 23*45);
+//      it('check that the product of 23 and 45 is 23*45', function (){
+//           // inside here assert is used
+//           assert.equal(multiply(23,45), 23*45);
      
-     });
-   })
+//      });
+//    })
 
 // Ampaire Irene
 // Atim Monica
-//    import {assert} from "chai";
-//    import factorial from "../factorial.js";
-//    import {describe, it} from "node:test";
+   import {assert} from "chai";
+   import factorial from "../factorial.js";
+   import {describe, it} from "mocha";
    
-//    describe('factorial of a number', function(){
+   describe('factorial of a number', function(){
        
-//        it('check that the factorial of 4 is 8', function (){
-//             // inside here assert is used
-//             assert.equal(factorial(4), 24);
+       it('check that the factorial of 4 is 8', function (){
+            // inside here assert is used
+            assert.equal(factorial(4), 24);
    
-//        });
-//      })
+       });
+     })
 
      // Ampaire Irene
      // Atim Monica
       //   import {assert} from "chai";
       //   import fibonacci from "../fibonacci.js";
-      //   import {describe, it} from "node:test";
+      //   import {describe, it} from "mocha";
         
       //   describe('fibonacci of a number', function(){
             
