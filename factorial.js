913eb30b9129969ca9fe152a0a4219423346a583
Ampaire Irene <ampaireirene7@gmail.com>
2025-04-05 12:56:33 +0300

    updated factorial function using recursion

--- factorial.js
+++ factorial.js
@@ -1,48 +1,56 @@
 // // Ampaire Irene
 // // Atim Monica
 
-function factorial (number){
+// function factorial (number){
 
-    if (typeof number !== 'number' || isNaN(number)) {
-        return undefined;
-    }
-// checking if number is negative
-if (number < 0) {
-    return undefined;
+//     if (typeof number !== 'number' || isNaN(number)) {
+//         return undefined;
+//     }
+// // checking if number is negative
+// if (number < 0) {
+//     return undefined;
     
-}
+// }
 
-// if number is 0
-else if (number === 0) {
-    return 1;
-}
+// // if number is 0
+// else if (number === 0) {
+//     return 1;
+// }
 
-// if number is positive
-else {
-    let fact = 1;
-    for (let i = 1; i <= number; i++) {
-        fact *= i;
-    }
-    return fact;
-}
-}
+// // if number is positive
+// else {
+//     let fact = 1;
+//     for (let i = 1; i <= number; i++) {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+// }
 
 // Ampaire Irene
 // Atim Monica
 
 // factorial using recursion(function calling a function)
-// function factorial(n){
-//     if (n==0)
-//         {
-//             console.log(`The factorial of ${number} is 1.`);
-//             }
+function factorial(number){
+    if (typeof number !== 'number' || isNaN(number)) {
+                return undefined;
+            }
+        // checking if number is negative
+        if (number < 0) {
+            return undefined;
             
-//     else {
-//         let fact = n*factorial(n-1);
+        }
+    if (number===0)
+        {
+            return 1;
+            }
+            
+    else {
+        let fact = number*factorial(number-1);
+        return fact;
 
-// console.log(`The factorial of ${number} is ${fact}.`);
-// } ;
+} ;
 
-// }
+}
 export default factorial;
 
